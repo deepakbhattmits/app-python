15c761737beed91aa70f56b6ca66eff5195c8aca
deepakbhattmits <deepakbhattmits@gmail.com>
2022-12-31 13:01:32 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+#Initializing list variable
+list=[1,2,3,4,5]
+products=[]
+
+
+def get_input():
+    '''Ask for user enter any product '''
+    val = input('Please enter your product : ')
+    return  val
+
+# using for loop
+for element in list:
+    print(element,'->',get_input())
