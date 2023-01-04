5c39d2294f662ed3b173aa68c02d2a0f6a03cac6
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-04 10:57:10 +0530

    Create tuple.py
    Added code for Tuple.

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# A tuple is a built-in data structure in Python that is an ordered collection of objects. Unlike lists, tuples come with limited functionality.
+
+# The primary differing characteristic between lists and tuples is mutability. Lists are mutable, whereas tuples are immutable. 
+# this is the use case of person name, age, here we can omit flower braces
+
+tuple_without_braces ='Deepak',29
+print('',tuple_without_braces)
+
+#  if we want to use flower braces lets say we want to send only onw value then we can use ()
+tuple_with_braces = ('Puji ', )
+print(tuple_with_braces)
