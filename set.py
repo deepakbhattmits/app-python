c5c6e7d4f61f0fbeadde17ed3f3c9d8b1f67ec9c
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-04 10:56:54 +0530

    Create set.py
    Added code for set()

---
+++ set.py
@@ -0,0 +1,10 @@
+# Sets are used to store multiple items in a single variable.
+# A set is a collection which is unordered, unchangeable*, and unindexed.
+# * Note: Set items are unchangeable, but you can remove items and add new items.
+# Set do not allow duplicate values. 
+list =['A','B','C','A','C']
+print(set(list))
+
+# we can use curly braces like a dictionary, however we don't enter key value pairs but values this automatically understood by python this is a set variable
+list1 ={'ABC','ABCD','CDE','ABC'}
+print(list1)
