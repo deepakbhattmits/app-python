6911ce0108ad734e7706a499ff9226806d007833
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-07 22:20:29 +0530

    Update dict_comprehensions.py

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,4 +1,5 @@
-list =[('ABC',29),('XYZ',30),('MNO',31)]
+# list =[('ABC',29),('XYZ',30),('MNO',31)]
+list =[{'name','XYZ'},{'age',30},{'address','NS street'}]
 
 dict_comprehension = {key :value for (key, value) in list}
 
