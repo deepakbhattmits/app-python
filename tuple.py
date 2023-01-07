a86dd907893c64319181d9176a4bf1450b65d900
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-07 22:21:15 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -9,3 +9,17 @@ print('',tuple_without_braces)
 #  if we want to use flower braces lets say we want to send only onw value then we can use ()
 tuple_with_braces = ('Puji ', )
 print(tuple_with_braces)
+
+list_comp = [el for el in tuple_with_braces ]
+print(' list comp : ',list_comp)
+for el in tuple_with_braces:
+    print(el)
+else:
+    print('Done loop')
+tuple_for_indexing=('ABC','XYZ','MNB')
+print(' Indexing: ',tuple_for_indexing[0])
+a,b,c = tuple_for_indexing
+print('Unpacking : \n')
+print('a', a)
+print('b', b)
+print('c', c)
