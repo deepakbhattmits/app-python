9e74d2dfe5394ed810e65814359aebc58fe0e828
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-07 22:21:12 +0530

    Create string_list.py

---
+++ string_list.py
@@ -0,0 +1,13 @@
+list = ['h','e','l','l','o']
+text_str = 'hello'
+# we will get Help on list object:
+# print(help(list))
+# Help on class str in module builtins:
+# print(help(str))
+for el in text_str:
+    print(el)
+else:
+    print('**'*20)
+
+text_str='cello'
+print(text_str)
