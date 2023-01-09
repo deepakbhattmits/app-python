c842d9e5685c96e0c10f8245eb612e931745cf24
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-09 11:07:49 +0530

    Update string_format.py

--- string_format.py
+++ string_format.py
@@ -22,3 +22,15 @@ print('Funds : {:>10.1f}'.format(funds))#Funds :      124.0 > align funds right
 print('Funds : {:<10.1f}'.format(funds))#Funds :124.0 < align funds left
 print('Funds : {:^10.1f}'.format(funds))#Funds :   124.0 ^ align funds center
 print('Funds : {:*^10.1f}'.format(funds))#Funds :   124.0 fill * symbol in empty space ^ align funds center
+
+format_string_f= f'I am {name} and I\'m {age} years old'
+
+format_string_f_with_float= f'I am {name} and I\'m {age:.2f} years old'
+print('format_string_f : ', format_string_f)
+print('format_string_f_with_float : ', format_string_f_with_float)
+person = {'name': 'Eric', 'age': 74}
+print("Hello, {name}. You are {age}.".format(** person))
+
+list =[1,2,3,4,5]
+
+print("List have {}, {}, {} and {}".format(*list))
